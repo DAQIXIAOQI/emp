b22abfebcd50fdd18d0285347d3ba7a653231794
zzQIjiEcoNG <qijiecongcong@163.com>
2021-11-08 10:53:31 +0800

    feat: 增加css编译loader

--- packages/emp/src/webpack/css.ts
+++ packages/emp/src/webpack/css.ts
@@ -1,6 +1,75 @@
 import store from 'src/helper/store'
 import wpChain from 'src/helper/wpChain'
-export const wpCSS = () => {
-  const config = {}
+export const wpCSS = (env: string | undefined) => {
+  const config = {
+    module: {
+      rule: {
+        css: {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: {
+            style: {
+              loader: require.resolve('style-loader'),
+            },
+            css: {
+              loader: require.resolve('css-loader'),
+              options: {
+                modules: {
+                  localIdentName: '[local]_[hash:base64:5]',
+                },
+              },
+            },
+          },
+        },
+        sass: {
+          test: /\.(scss|sass)$/,
+          exclude: /node_modules/,
+          use: {
+            style: {
+              loader: require.resolve('style-loader'),
+            },
+            css: {
+              loader: require.resolve('css-loader'),
+              options: {
+                modules: {
+                  localIdentName: '[local]_[hash:base64:5]',
+                },
+              },
+            },
+            sass: {
+              loader: require.resolve('sass-loader'),
+              options: {
+                implementation: require('sass'),
+                sourceMap: env === 'development',
+              },
+            },
+          },
+        },
+        less: {
+          test: /\.less$/,
+          exclude: /node_modules/,
+          use: {
+            style: {
+              loader: require.resolve('style-loader'),
+            },
+            css: {
+              loader: require.resolve('css-loader'),
+              options: {
+                modules: {
+                  localIdentName: '[local]_[hash:base64:5]',
+                },
+              },
+            },
+            less: {
+              loader: require.resolve('less-loader'),
+              options: {
+                lessOptions: {javascriptEnabled: true},
+              },
+            },
+          },
+        },
+      },
+    },
+  }
   wpChain.merge(config)
 }
--- projects/demo/src/App.tsx
+++ projects/demo/src/App.tsx
@@ -1,2 +1,8 @@
+import React from 'react'
+import Styles from './App.css'
+import Scss from './App.scss'
+
+console.log(Styles, Scss)
+
 const App = () => <h1>EMP V2.0!</h1>
 export default App
